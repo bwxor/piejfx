eeff7d2c0bbda3c1056cbebc7328217655ffbf37
mateasmario <mmmateas@gmail.com>
2026-01-04 11:48:23 +0200

    Added dependencies to javafx.web

--- piejfx/src/main/java/module-info.java
+++ piejfx/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.bwxor.piejfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
