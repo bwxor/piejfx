b119ff3f4f2bd8314e101ef8d7bf0f53e3471bf6
Mario Mateas <mateasmario@aol.com>
2026-05-01 22:44:48 +0300

    Added stage owner to notifications for better integration with Linux window managers

--- piejfx/src/main/java/com/bwxor/piejfx/service/NotificationService.java
+++ piejfx/src/main/java/com/bwxor/piejfx/service/NotificationService.java
@@ -4,6 +4,7 @@ import com.bwxor.piejfx.constants.AppDirConstants;
 import com.bwxor.piejfx.controller.impl.NotificationOkViewController;
 import com.bwxor.piejfx.controller.impl.NotificationYesNoCancelViewController;
 import com.bwxor.piejfx.state.ServiceState;
+import com.bwxor.piejfx.state.StageState;
 import com.bwxor.piejfx.state.ThemeState;
 import com.bwxor.plugin.service.PluginNotificationService;
 import com.bwxor.plugin.type.NotificationYesNoCancelOption;
@@ -12,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -22,6 +24,7 @@ import java.util.Objects;
 public class NotificationService implements PluginNotificationService {
     public void showNotificationOk(String notificationText) {
         ServiceState serviceState = ServiceState.instance;
+        StageState stageState = StageState.instance;
 
         FXMLLoader loader = new FXMLLoader(serviceState.getResourceService().getResourceByName("views/notification-ok-view.fxml"));
         Parent root;
@@ -43,6 +46,9 @@ public class NotificationService implements PluginNotificationService {
 
         stage.getIcons().add(new Image(Objects.requireNonNull(serviceState.getResourceService().getResourceByNameAsStream("img/icons/icon.png"))));
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        stage.initOwner(stageState.getStage());
         scene.setFill(Color.TRANSPARENT);
         try {
             scene.getStylesheets().add(AppDirConstants.DEFAULT_STYLES_FILE.toUri().toURL().toExternalForm());
@@ -55,6 +61,7 @@ public class NotificationService implements PluginNotificationService {
 
     public NotificationYesNoCancelOption showNotificationYesNoCancel(String notificationText) {
         ServiceState serviceState = ServiceState.instance;
+        StageState stageState = StageState.instance;
 
         FXMLLoader loader = new FXMLLoader(serviceState.getResourceService().getResourceByName("views/notification-yesnocancel-view.fxml"));
         Parent root;
@@ -78,6 +85,9 @@ public class NotificationService implements PluginNotificationService {
         });
         stage.getIcons().add(new Image(Objects.requireNonNull(serviceState.getResourceService().getResourceByNameAsStream("img/icons/icon.png"))));
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        stage.initOwner(stageState.getStage());
         scene.setFill(Color.TRANSPARENT);
         try {
             scene.getStylesheets().add(AppDirConstants.DEFAULT_STYLES_FILE.toUri().toURL().toExternalForm());
